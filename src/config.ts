6853aba5b27c6990cb78ae8d7497419b78eeac18
psvm203 <psvm203@proton.me>
2026-05-31 18:22:25 +0900

    chore: remove default configs

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { ExpressiveCodeConfig, LicenseConfig, NavBarConfig, ProfileConfig, 
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "Fuwari",
-  subtitle: "Demo Site",
-  lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+  title: "psvm203",
+  subtitle: "blog",
+  lang: "ko", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
   themeColor: {
     hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false, // Hide the theme color picker for visitors
@@ -40,39 +40,27 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: "GitHub",
-      url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+      url: "https://github.com/psvm203", // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
     },
   ],
 };
 
 export const profileConfig: ProfileConfig = {
-  avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: "Lorem Ipsum",
-  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: "psvm203",
+  bio: "호기심 많은 개발자",
   links: [
-    {
-      name: "Twitter",
-      icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-      // You will need to install the corresponding icon set if it's not already included
-      // `pnpm add @iconify-json/<icon-set-name>`
-      url: "https://twitter.com",
-    },
-    {
-      name: "Steam",
-      icon: "fa6-brands:steam",
-      url: "https://store.steampowered.com",
-    },
     {
       name: "GitHub",
       icon: "fa6-brands:github",
-      url: "https://github.com/saicaca/fuwari",
+      url: "https://github.com/psvm203",
     },
   ],
 };
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
+  enable: false,
   name: "CC BY-NC-SA 4.0",
   url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
 };
