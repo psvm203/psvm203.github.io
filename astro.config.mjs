38614eca530f2be18ebda921598c91966e94db4b
psvm203 <psvm203@proton.me>
2026-06-01 10:30:06 +0900

    style: remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 export default defineConfig({
   site: "https://psvm203.github.io/",
   base: "/",
-  trailingSlash: "always",
+  trailingSlash: "never",
   integrations: [
     tailwind({
       nesting: true,
