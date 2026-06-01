2a3b0da34f35a341481afd95f79c772871b2d957
psvm203 <psvm203@proton.me>
2026-06-01 15:40:15 +0900

    style: show language on expressive code

--- src/plugins/expressive-code/language-badge.ts
+++ src/plugins/expressive-code/language-badge.ts
@@ -1,49 +1,45 @@
-/**
- * Based on the discussion at https://github.com/expressive-code/expressive-code/issues/153#issuecomment-2282218684
- */
 import { definePlugin } from "@expressive-code/core";
+import { h } from "hastscript";
 
 export function pluginLanguageBadge() {
   return definePlugin({
     name: "Language Badge",
-    // @ts-expect-error
-    baseStyles: ({ _cssVar }) => `
-      [data-language]::before {
-        position: absolute;
-        z-index: 2;
-        right: 0.5rem;
-        top: 0.5rem;
-        padding: 0.1rem 0.5rem;
-        content: attr(data-language);
-        font-family: "JetBrains Mono Variable", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
-        font-size: 0.75rem;
-        font-weight: bold;
-        text-transform: uppercase;
-        color: oklch(0.75 0.1 var(--hue));
-        background: oklch(0.33 0.035 var(--hue));
-        border-radius: 0.5rem;
-        pointer-events: none;
-        transition: opacity 0.3s;
-        opacity: 0;
+    hooks: {
+      postprocessRenderedBlock({ codeBlock, renderData }) {
+        const lang = codeBlock.language;
+        if (!lang) return;
+
+        const figure = renderData.blockAst;
+        const header = figure.children?.find(
+          (child) => child.type === "element" && child.tagName === "figcaption",
+        );
+
+        if (!header) return;
+
+        // @ts-expect-error
+        header.children.push(h("span.language-badge", lang));
+      },
+    },
+    baseStyles: `
+      .header {
+        position: relative;
       }
-      .frame:not(.has-title):not(.is-terminal) {
-        @media (hover: none) {
-          & [data-language]::before {
-            opacity: 1;
-            margin-right: 3rem;
-          }
-          & [data-language]:active::before {
-            opacity: 0;
-          }
-        }
-        @media (hover: hover) {
-          & [data-language]::before {
-            opacity: 1;
-          }
-          &:hover [data-language]::before {
-            opacity: 0;
-          }
-        }
+      .language-badge {
+        position: absolute;
+        left: 4rem;
+        top: 50%;
+        transform: translateY(-50%);
+        font-family: "JetBrains Mono Variable", ui-monospace, SFMono-Regular, Menlo,
+          Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+        font-size: 0.7rem;
+        font-weight: 600;
+        text-transform: uppercase;
+        letter-spacing: 0.06em;
+        color: oklch(0.65 0.12 var(--hue));
+        pointer-events: none;
+      }
+      .has-title .language-badge {
+        display: none;
       }
     `,
   });
